8202a4e0c34abebc92f01755498981570c856009
Oswar Baez <91704080+Orlando2601@users.noreply.github.com>
2022-01-28 13:03:07 -0500

    update comments

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,4 +1,4 @@
-/* IMPORTACION DE MODULOS */
+/* IMPORTACION DE MODULOS ////////////////////////////////////////////////////////////////////////////*/
 const express = require('express'); /* Importamos modulo express */
 const router = express.Router(); /* Definimos el método Router de express a la variable router para exportarla */
 const userController = require('../controllers/userController');
